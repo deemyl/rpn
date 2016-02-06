f1dee317b2b87cf2b6f8298cb61c70f280c1a287
deemyl <deepamylangam@Deepas-MacBook-Pro.local>
2016-02-06 16:34:18 +1100

    implemented a version — need more unit testing

--- src/main/java/com/test/anz/App.java
+++ src/main/java/com/test/anz/App.java
@@ -1,6 +1,10 @@
 package com.test.anz;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EmptyStackException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
@@ -15,58 +19,154 @@ import org.apache.commons.lang.StringUtils;
 public class App 
 {
 	private static final String INSUFF_PARAMETERS_STR = ": insufficient parameters";
-	private static final String MSG_INSTRUCTION = "Please enter digits and operands such as +,-,*,/,sqrt,clear,undo with spaces between.\n";
 	private static final String OPERATOR_STR = "operator";
 	private static final String SPACE_STR = " ";
-	private static final String POSITION_STR = "position";
+	private static final String POSITION_STR = "position:";
 	private static final String OPEN_BRACKET_STR = "(";
 	private static final String CLOSE_BRACKET_STR = ")";
 	private static final String STACK_STR = "stack:";
 	private static final String OPERAND_REGEX = "[-+*/]";
-	private static final String REAL_NUMBERS_REGEX = "\\d+";	//TODO:NEED TO TEST FOR ALL REAL NUMBERS
+	private static final String COMMAND_REGEX = "(?i:sqrt)|(?i:undo)|(?i:clear)";
+	private static final String REAL_NUMBERS_REGEX = "^\\d+(\\.\\d+)?$";	//TODO:NEED TO TEST FOR ALL REAL NUMBERS
 	
-	private static Stack<String> stack = new Stack<String>();
+	protected Stack<String> stack = new Stack<String>();
 	
 	public static void main( String[] args )
     {
         App rpnApp = new App();
-        rpnApp.validateInput();
-        
+        rpnApp.runCalculator();
     }
     
-    private List<String> validateInput() {
-    	String input = null;
-    	Scanner in = null;
-    	while(StringUtils.isEmpty(input)) {
-    		System.out.println(MSG_INSTRUCTION);
-        	in = new Scanner(System.in);
-            input = in.nextLine().trim();
-    	}
-    	in.close();
-        List<String> inputList = Arrays.asList(input.split(" "));
-    	if(inputList.isEmpty() || inputList.size() <= 1) {
-    		printIncorrectParameterMsg(inputList.get(0), 0);
-    		System.exit(-1);
+	protected void runCalculator() {
+		List<String> completeInputList = new ArrayList<String>();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+	        int size = completeInputList.size();
+	        String input = sc.nextLine().trim();
+	        List<String> inputList = validateInputString(input);
+	        completeInputList.addAll(inputList);
+	        createStack(completeInputList, size);	        
+        }
+	}
+	
+    protected List<String> validateInputString(String input) {
+    	List<String> inputList = new ArrayList<String>();
+    	if(StringUtils.isEmpty(input.trim())) {
+    		printIncorrectParameterMsg(" ", 0);
+    	} else {
+    		inputList = Arrays.asList(input.split(" "));
+	    	if(inputList.isEmpty()) { 
+	    		printIncorrectParameterMsg(inputList.get(0), 0);
+	    	}
     	}
     	return inputList;
     }
     
-    private void createStack(List<String> inputList) {
-    	for(int i = 0; i < inputList.size(); i++) {
-    		//if(inputList.get(i) )
-    		//stack.push(inputList.get(i));
+    protected void createStack(List<String> inputList, int index) {
+    	String prevValinStack = null;
+    	String currValinStack = null;
+    	for(int i = index; i < inputList.size(); i++) {
+    		int pointerPos = i - index;
+    		String currVal = inputList.get(i);
+    		if(!validateEntry(currVal)) {
+    			printIncorrectParameterMsg(currVal, pointerPos);
+    			break;
+    		}
+    		if(!isNumber(currVal)) {
+    			currValinStack = peek();
+    			if(currValinStack == null) {
+    				printIncorrectParameterMsg(currVal, pointerPos);
+    				break;
+    			}    		
+	    		if(isOperand(currVal)) {
+	    			currValinStack = stack.pop();
+	    			prevValinStack = peek();
+    				if(prevValinStack == null) {
+    					printIncorrectParameterMsg(currVal, pointerPos);
+    					stack.push(currValinStack);
+        				break;
+    				} else {
+    					stack.push(doOperation(stack.pop(), currValinStack, currVal));
+    					continue;
+    				}	
+	    		} else if(isSquareRootCommand(currVal)) {
+	    			currValinStack = stack.pop();
+	    			String result = squareroot(currValinStack);
+    				stack.push(result);
+	    		} else if(isUndoCommand(currVal)) {
+	    			String prevVal = inputList.get(i-1);
+	    			if(isNumber(prevVal)) {
+	    				stack.pop();
+	    			} else if(isOperand(prevVal)) {
+	    				if(i >= 3) {
+	    					stack.pop();
+	    					stack.push(inputList.get(i-3));
+	    					stack.push(inputList.get(i-2));
+	    				} else {
+	    					//incorrect error
+	    					break;
+	    				}
+	    			} else if(isSquareRootCommand(prevVal)) {
+	    				stack.pop();
+	    				stack.push(inputList.get(i-2));
+	    			} else if(isUndoCommand(prevVal)) {
+	    				if(peek() == null) {
+	    					printIncorrectParameterMsg(currVal, pointerPos);
+	    					break;
+	    				} else {
+	    					stack.pop();
+	    				}
+	    			} else { 
+	    				continue;
+	    			}
+	    		} else if(isClearCommand(currVal)) {
+	        		stack.clear();
+	        	} else {
+	        		//incorrect command it is not number or the known operations
+	        	}
+    		} else if(isNumber(currVal)) {
+    			stack.push(currVal);
+    		} else {
+    			System.out.println("ERROR");
+    		}
     	}
+    	printStack();
     }
     
-    private boolean isOperand(String val) {
+    protected boolean validateEntry(String val) {
+    	return isNumber(val) || isKnownOperation(val);
+    }
+    
+    protected boolean isKnownOperation(String val) {
+    	return val.matches(OPERAND_REGEX + "|" + COMMAND_REGEX);
+    }
+    
+    protected boolean isOperand(String val) {
     	return val.matches(OPERAND_REGEX);
     }
     
-    private boolean isNumber(String val) {
+    protected boolean isSquareRootCommand(String val) {
+    	return val.matches("(?i:sqrt)");
+    }
+    
+    protected boolean isClearCommand(String val) {
+    	return val.matches("(?i:clear)");
+    }
+    
+    protected boolean isUndoCommand(String val) {
+    	return val.matches("(?i:undo)");
+    }
+    
+    protected boolean isNumber(String val) {
     	return val.matches(REAL_NUMBERS_REGEX);
     }
     
     private void printIncorrectParameterMsg(String val, int position) {
+    	if(position == 0) {
+    		position = 1;
+    	} else {
+    		position = position*2 + 1;	//add the space position
+    	}
     	StringBuffer sb = new StringBuffer();
 		sb.append(OPERATOR_STR).append(SPACE_STR).append(val).append(SPACE_STR);
 		sb.append(OPEN_BRACKET_STR).append(POSITION_STR).append(SPACE_STR).append(position);
@@ -74,44 +174,40 @@ public class App
 		System.out.println(sb.toString());
     }
     
-    private void printStack(Stack<String> stck) {
+    private void printStack() {
     	StringBuffer sb = new StringBuffer();
     	sb.append(STACK_STR).append(SPACE_STR);
-    	for (int i = 0; i <stck.size(); i++) {
-    		sb.append(stck.get(i)).append(SPACE_STR);    		
+    	for (int i = 0; i < stack.size(); i++) {
+    		sb.append(stack.get(i)).append(SPACE_STR);    		
     	}
     	System.out.println(sb.toString().trim());
     }
     
-    private void pop() {
-    	
+    protected String peek() {
+    	try {
+    		return stack.peek();
+    	} catch(EmptyStackException e) {
+    		//since there is nothing in the stack
+    		return null;
+    	}
     }
     
-    private void multiply() {
-    	
+    protected String squareroot(String val) {
+    	return (new BigDecimal(Math.sqrt(convertString(val).doubleValue()), new MathContext(15))).toString();
     }
     
-    private void divide() {
-    	
+    protected BigDecimal convertString(String val) {
+    	return new BigDecimal(val, new MathContext(15));
     }
     
-    private void add() {
-    	
-    }
-    
-    private void subtract() {
-    	
-    }
-    
-    private void undo() {
-    	
-    }
-    
-    private void clear() {
-    	
-    }
-    
-    private void squareroot() {
-    	
+    protected String doOperation(String val1, String val2, String operation) {   	
+    	switch(operation) {
+    		case "+":		return convertString(val1).add(convertString(val2)).toString();
+    		case "-":		return convertString(val1).subtract(convertString(val2)).toString();
+    		case "/":		return convertString(val1).divide(convertString(val2)).toString();
+    		case "*":		return convertString(val1).multiply(convertString(val2)).toString();
+    			
+    		default:		return null;
+    	}
     }
 }
--- src/test/java/com/test/anz/AppTest.java
+++ src/test/java/com/test/anz/AppTest.java
@@ -1,5 +1,7 @@
 package com.test.anz;
 
+import java.math.BigDecimal;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -38,23 +40,65 @@ public class AppTest
     }
     
     public void testIsOperand() {
-    	String pattern = "[-+*/]";
+    	String pattern = "[-+*/]|(?i:sqrt)|(?i:undo)|(?i:clear)";
     	assertFalse("111".matches(pattern));
     	assertFalse("+-".matches(pattern));
-    	assertFalse("".matches(pattern));
     	assertFalse(" ".matches(pattern));
     	assertFalse("sdf".matches(pattern));
+    	assertFalse("1*".matches(pattern));
     	assertTrue("+".matches(pattern));
     	assertTrue("-".matches(pattern));
     	assertTrue("/".matches(pattern));
     	assertTrue("*".matches(pattern));
+    	assertFalse("[".matches(pattern));
+    	assertTrue("sqrt".matches(pattern));
+    	assertTrue("SQRT".matches(pattern));
+    	assertFalse("sqrt+".matches(pattern));
+    	assertFalse("sqrtclear".matches(pattern));
     	
     }
     
     public void testIsNumber() {
-    	String pattern = "\\d+";
+    	String pattern = "^\\d+(\\.\\d+)?$";
     	assertTrue("111".matches(pattern));
+    	assertFalse("1*".matches(pattern));
     	assertFalse("1a1".matches(pattern));
     	assertTrue("1.11111111".matches(pattern)); //???? GOT TO FIX THIS PATTERN 
+    }
+    
+    public void testValidateInputString() {
+    	assertTrue(rpnApp.validateInputString(" ").isEmpty());
+    	assertTrue(rpnApp.validateInputString("").isEmpty());
+    	assertFalse(rpnApp.validateInputString("1*").isEmpty());
+    	assertTrue(rpnApp.validateInputString("1*").size() == 1);
+    }
+    
+    public void testValidateEntry() {
+    	assertFalse(rpnApp.validateEntry("1*"));
+    	assertFalse(rpnApp.validateEntry("**"));
+    	assertFalse(rpnApp.validateEntry("*/"));
+    	assertFalse(rpnApp.validateEntry("$%"));
+    	assertFalse(rpnApp.validateEntry("sqrtclear"));
+    	assertTrue(rpnApp.validateEntry("*"));
+    	assertTrue(rpnApp.validateEntry("145.23253"));
+    	assertTrue(rpnApp.validateEntry("sqrt"));
+    	assertTrue(rpnApp.validateEntry("clear"));
+    	assertTrue(rpnApp.validateEntry("undo"));
+    }
+    
+    public void testDoOperation() {
+    	String expResult = "4";
+    	assertNull(rpnApp.doOperation("1", "2", "%"));
+    	assertTrue(rpnApp.doOperation("2", "2", "+").equals(expResult));
+    }
+    
+    public void testConvertString() {
+    	String testStr = "5";
+    	BigDecimal expRes = new BigDecimal(testStr);
+    	assertEquals(expRes, rpnApp.convertString(testStr));
+    	
+    	testStr = "5.666666666666666666666666";
+    	assertEquals(new BigDecimal("5.66666666666667"), rpnApp.convertString(testStr));		//testing the precision upto 15
+    	
     }
 }
